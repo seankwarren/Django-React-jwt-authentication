8634f1c70ef530ff4f0755bc2d968cf575ea425a
KumarAjit <ajit.megaproject@gmail.com>
2024-08-26 12:41:07 +0900

    Update AuthContext.js
    Updated import for jwtDecode for latest version.

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from 'react'
-import jwtDecode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 import { useNavigate } from 'react-router-dom'
 
 const AuthContext = createContext()
